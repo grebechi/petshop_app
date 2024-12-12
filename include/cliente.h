5e1c54f8f744bbc7828272d1977ad5198628ac68
Gabriel Rebechi <bibo.rebechi@gmail.com>
2024-12-12 16:10:05 -0300

    Estrutura: adiciona a pasta include que contém todos os arquivos de cabeçalho

---
+++ include/cliente.h
@@ -0,0 +1,28 @@
+#ifndef CLIENTE_H
+#define CLIENTE_H
+
+// Tamanho máximo para os campos
+#define CLIENTE_MAX_NOME 50
+#define CLIENTE_MAX_TELEFONE 15
+#define CLIENTE_MAX_CPF 15
+
+// Estrutura do Cliente
+typedef struct {
+    int id;
+    char nome[CLIENTE_MAX_NOME];
+    char telefone[CLIENTE_MAX_TELEFONE];
+    char cpf[CLIENTE_MAX_CPF];
+} Cliente;
+
+// Declaração das funções
+int gerarProximoID();
+void salvarCliente(Cliente cliente);
+int contarClientes();
+Cliente *listarClientes(int quantidade);
+int atualizarCliente(Cliente *clienteRecebido);
+int excluirCliente(int id);
+Cliente *buscarClientePeloID(int id);
+int clienteJaExiste(const char *nome);
+void adicionarClientesPadrao();
+
+#endif
---
+++ include/utils.h
@@ -0,0 +1,10 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+// Função para limpar a tela
+void limparTerminal();
+
+// Função para pausar o terminal
+void pausarTerminal();
+
+#endif
